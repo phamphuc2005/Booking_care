b5f39afda34d6726b22da0c35c3945cbd72efcdb
phamphuc2005 <minhpham2001bk@gmail.com>
2022-10-15 15:25:00 +0700

    delete users

--- booking_care_be/src/controllers/homeController.js
+++ booking_care_be/src/controllers/homeController.js
@@ -25,7 +25,7 @@ let postCRUD = async (req, res) => {
 
 let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUser();
-    console.log(data);
+    // console.log(data);
     return res.render('displayCRUD.ejs', {
         dataTable: data
     });
@@ -53,6 +53,16 @@ let putCRUD = async (req, res) => {
     }); 
 }
 
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if(id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Deleted user!');        
+    } else {
+        return res.send('User not found!');        
+    }
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
@@ -60,4 +70,5 @@ module.exports = {
     displayGetCRUD: displayGetCRUD,
     getEditCRUD: getEditCRUD,
     putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
