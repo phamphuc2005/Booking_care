932d16d129e59b18d848a665597dfc37d2f2be89
phamphuc2005 <minhpham2001bk@gmail.com>
2022-10-18 22:12:32 +0700

    show users api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,6 +20,26 @@ let handleLogin = async (req, res) => {
     })
 }
 
+let handleGetAllUsers = async (req, res) => {
+    let id = req.query.id; //ALL, ID
+    let users = await userService.getAllUsers(id);
+
+    if(!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameters',
+            users: []
+        })
+    }
+
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'OK',
+        users
+    })
+}
+
 module.exports = {
     handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers,
 }
