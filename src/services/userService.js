d841056f1fdfd1e95b9b3282d957e4ebcbe4fef8
phamphuc2005 <minhpham2001bk@gmail.com>
2022-11-05 16:46:47 +0700

    fix login api

--- src/services/userService.js
+++ src/services/userService.js
@@ -22,7 +22,7 @@ let handleUserLogin = (email, password) => {
             if (isExist) {
                 let user = await db.User.findOne({
                     where: {email: email},
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                     raw: true
                 });
                 if (user) {
