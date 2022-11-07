4beca9e13cf99afc290e8b6602a8020ad40630b3
phamphuc2005 <minhpham2001bk@gmail.com>
2022-11-07 22:58:10 +0700

    update user model

--- src/services/userService.js
+++ src/services/userService.js
@@ -113,10 +113,10 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phonenumber: data.phonenumber,
-                    gender: data.gender === '1' ? true : false,
+                    gender: data.gender,
                     roleId: data.roleId,
+                    positionId: data.positionId,
                     // image: data.image,
-                    // positionId: data.positionId,
                 })  
                 resolve({
                     errCode: 0,
