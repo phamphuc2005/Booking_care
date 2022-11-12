9de45e986f839c90af14f92a884046110348eb95
phamphuc2005 <minhpham2001bk@gmail.com>
2022-11-12 16:19:39 +0700

    get detail doctor by Id api

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -73,8 +73,41 @@ let saveInfoDoctor = (inputData) => {
     })
 }
 
+let getDetailDoctorById = (inputId) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            if(!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameters!'
+                })
+            } else {
+                let data = await db.User.findOne({
+                    where: { id: inputId},
+                    attributes: {
+                        exclude: ['password', 'image']
+                    },
+                    include: [
+                        {model: db.Markdown},
+                        {model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi']}
+                    ],
+                    raw: true,
+                    nest: true
+                })
+                resolve({
+                    errCode: 0,
+                    data: data
+                })
+            }
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
-    saveInfoDoctor: saveInfoDoctor
+    saveInfoDoctor: saveInfoDoctor,
+    getDetailDoctorById: getDetailDoctorById
 }
