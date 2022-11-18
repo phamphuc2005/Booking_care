bdb0bb057f5af119bceb7526dffd7d50032cd2f3
phamphuc2005 <minhpham2001bk@gmail.com>
2022-11-18 22:58:27 +0700

    create doctor_info mpdel, update save doctor info api

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -54,7 +54,9 @@ let getAllDoctors = () => {
 let saveInfoDoctor = (inputData) => {
     return new Promise(async(resolve, reject) => {
         try {
-            if(!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown || !inputData.action) {
+            if(!inputData.doctorId || !inputData.contentHTML || !inputData.contentMarkdown || !inputData.action ||
+                !inputData.selectedPrice || !inputData.selectedPayment || !inputData.selectedProvince ||
+                !inputData.nameClinic || !inputData.addressClinic || !inputData.note) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing parameters!'
@@ -81,6 +83,35 @@ let saveInfoDoctor = (inputData) => {
                         await doctorMarkdown.save()
                     }
                 }
+
+                let doctorInfo = await db.Doctor_Info.findOne({
+                    where: {
+                        doctorId: inputData.doctorId
+                    },
+                    raw: false
+                })
+                if(doctorInfo) {
+                    doctorInfo.doctorId = inputData.doctorId;
+                    doctorInfo.priceId = inputData.selectedPrice;
+                    doctorInfo.paymentId = inputData.selectedPayment;
+                    doctorInfo.provinceId = inputData.selectedProvince;
+                    doctorInfo.nameClinic = inputData.nameClinic;
+                    doctorInfo.addressClinic = inputData.addressClinic;
+                    doctorInfo.note = inputData.note;
+
+                    await doctorInfo.save()
+                } else {
+                    await db.Doctor_Info.create({
+                        doctorId: inputData.doctorId,
+                        priceId: inputData.selectedPrice,
+                        paymentId: inputData.selectedPayment,
+                        provinceId: inputData.selectedProvince,
+                        nameClinic: inputData.nameClinic,
+                        addressClinic: inputData.addressClinic,
+                        note: inputData.note
+                    })
+                }
+
                 resolve({
                     errCode: 0,
                     errMessage: 'Save doctor info successfully!'
