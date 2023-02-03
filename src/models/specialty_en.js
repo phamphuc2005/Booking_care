a5448893a8ada55469f9befb85d125ee5b5e75f3
phamphuc2005 <minhpham2001bk@gmail.com>
2023-02-03 17:52:22 +0700

    update specialty api

---
+++ src/models/specialty_en.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Specialty_En extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Specialty_En.hasOne(models.Doctor_Info, {foreignKey: 'id'})
+
+    }
+  };
+  Specialty_En.init({
+    name: DataTypes.STRING,
+    descriptionMarkdown: DataTypes.TEXT,
+    descriptionHTML: DataTypes.TEXT,
+    image: DataTypes.TEXT,
+  }, {
+    sequelize,
+    modelName: 'Specialty_En',
+  });
+  return Specialty_En;
+};
--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -19,28 +19,60 @@ let checkSpecialtyName = (dataName) => {
     })
 }
 
+let checkSpecialtyName_en = (dataName) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let specialty = await db.Specialty_En.findOne({
+                where: {name: dataName}
+            })
+            if(specialty) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
 let createSpecialty = (data) => {
     return new Promise(async(resolve, reject) => {
         try {
-            if(!data.name || !data.imageBase64 || !data.descriptionHTML || !data.descriptionMarkdown) {
+            if(!data.name_vi || !data.imageBase64 || !data.descriptionHTML_vi || !data.descriptionMarkdown_vi ||
+                !data.name_en || !data.descriptionHTML_en || !data.descriptionMarkdown_en) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing required parameters!'
                 })
             } else {
-                let check = await checkSpecialtyName(data.name);
-                if(check === true) {
+                let check = await checkSpecialtyName(data.name_vi);
+                let check_en = await checkSpecialtyName_en(data.name_en);
+                if(check === true && check_en === true) {
                     resolve({
                         errCode: -1,
                         errMessage: 'Specialty has been used. Please enter another name!'
                     });
                 } else {
                     await db.Specialty.create({
-                        name: data.name,
+                        name: data.name_vi,
                         image: data.imageBase64,
-                        descriptionHTML: data.descriptionHTML,
-                        descriptionMarkdown: data.descriptionMarkdown
+                        descriptionHTML: data.descriptionHTML_vi,
+                        descriptionMarkdown: data.descriptionMarkdown_vi
                     })
+                    let specialty = await db.Specialty.findOne({
+                        where: {name: data.name_vi},
+                        raw: false
+                    })
+                    if(specialty){
+                        await db.Specialty_En.create({
+                            name: data.name_en,
+                            image: data.imageBase64,
+                            descriptionHTML: data.descriptionHTML_en,
+                            descriptionMarkdown: data.descriptionMarkdown_en,
+                            id : specialty.id,
+                        })
+                    }
                     resolve({
                         errCode: 0,
                         errMessage: 'OK!'
@@ -53,12 +85,19 @@ let createSpecialty = (data) => {
     })
 }
 
-let getAllSpecialty = (data) => {
+let getAllSpecialty = (data_vi, data_en) => {
     return new Promise(async(resolve, reject) => {
         try {
-            let data = await db.Specialty.findAll();
-            if (data && data.length > 0) {
-                data.map(item => {
+            let data_vi = await db.Specialty.findAll();
+            if (data_vi && data_vi.length > 0) {
+                data_vi.map(item => {
+                    item.image = new Buffer.from(item.image, 'base64').toString('binary');
+                    return item;
+                })
+            }
+            let data_en = await db.Specialty_En.findAll();
+            if (data_en && data_en.length > 0) {
+                data_en.map(item => {
                     item.image = new Buffer.from(item.image, 'base64').toString('binary');
                     return item;
                 })
@@ -66,7 +105,8 @@ let getAllSpecialty = (data) => {
             resolve({
                 errMessage: "OK!",
                 errCode: 0,
-                data
+                data_vi: data_vi,
+                data_en: data_en
             })
         } catch (error) {
             reject(error)
@@ -77,7 +117,8 @@ let getAllSpecialty = (data) => {
 let editSpecialty = (data) => {
     return new Promise(async(resolve, reject) => {
         try {
-            if(!data.id || !data.name || !data.imageBase64 || !data.descriptionHTML || !data.descriptionMarkdown) {
+            if(!data.id || !data.name_vi || !data.imageBase64 || !data.descriptionHTML_vi || !data.descriptionMarkdown_vi ||
+                !data.name_en || !data.descriptionHTML_en || !data.descriptionMarkdown_en) {
                 resolve({
                     errCode: 1,
                     errMessage: 'Missing required parameters!',
@@ -87,30 +128,47 @@ let editSpecialty = (data) => {
                     where: {id: data.id},
                     raw: false
                 })
-                if(specialty) {
-                    if(specialty.name == data.name) {
-                        specialty.name = data.name,
+                let specialty_en = await db.Specialty_En.findOne({
+                    where: {id: data.id},
+                    raw: false
+                })
+                if(specialty && specialty_en) {
+                    if(specialty.name == data.name_vi && specialty_en.name == data.name_en) {
+                        specialty.name = data.name_vi,
                         specialty.image = data.imageBase64,
-                        specialty.descriptionHTML = data.descriptionHTML,
-                        specialty.descriptionMarkdown = data.descriptionMarkdown;
+                        specialty.descriptionHTML = data.descriptionHTML_vi,
+                        specialty.descriptionMarkdown = data.descriptionMarkdown_vi;
                         await specialty.save();
+
+                        specialty_en.name = data.name_en,
+                        specialty_en.image = data.imageBase64,
+                        specialty_en.descriptionHTML = data.descriptionHTML_en,
+                        specialty_en.descriptionMarkdown = data.descriptionMarkdown_en;
+                        await specialty_en.save();
                         resolve({
                             errCode: 0,
                             message: 'Update specialty successfully!'
                         });
                     } else {
-                        let check = await checkSpecialtyName(data.name);
-                        if(check === true) {
+                        let check = await checkSpecialtyName(data.name_vi);
+                        let check_en = await checkSpecialtyName_en(data.name_en);
+                        if(check === true && check_en === true) {
                             resolve({
                                 errCode: -1,
                                 errMessage: 'Specialty has been used. Please enter another name!'
                             });
                         } else {
-                            specialty.name = data.name,
+                            specialty.name = data.name_vi,
                             specialty.image = data.imageBase64,
-                            specialty.descriptionHTML = data.descriptionHTML,
-                            specialty.descriptionMarkdown = data.descriptionMarkdown;
+                            specialty.descriptionHTML = data.descriptionHTML_vi,
+                            specialty.descriptionMarkdown = data.descriptionMarkdown_vi;
                             await specialty.save();
+
+                            specialty_en.name = data.name_en,
+                            specialty_en.image = data.imageBase64,
+                            specialty_en.descriptionHTML = data.descriptionHTML_en,
+                            specialty_en.descriptionMarkdown = data.descriptionMarkdown_en;
+                            await specialty_en.save();
                             resolve({
                                 errCode: 0,
                                 message: 'Update specialty successfully!'
@@ -142,13 +200,19 @@ let deleteSpecialty = (data) => {
                 let specialty = await db.Specialty.findOne({
                     where : {id: data.id},
                 })
-                if(!specialty) {
+                let specialty_en = await db.Specialty_En.findOne({
+                    where : {id: data.id},
+                })
+                if(!specialty || !specialty_en) {
                     resolve({
                         errCode: 2,
                         errMessage: 'Specialty does not exist!'
                     })
                 }
                 await db.Specialty.destroy({
+                    where : {id: data.id},
+                });
+                await db.Specialty_En.destroy({
                     where : {id: data.id},
                 });
                 resolve({
@@ -171,15 +235,15 @@ let getDetailSpecialtyById = (inputId, location) => {
                     errMessage: 'Missing required parameters!',
                 })
             } else {
-                let data = await db.Specialty.findOne({
+                let data_vi = await db.Specialty.findOne({
                     where : {id: inputId},
-                    // attributes: {
-                    //     exclude: ['']
-                    // },
+                })
+                let data_en = await db.Specialty_En.findOne({
+                    where : {id: inputId},
                 })
 
-                if(data) {
-                    data.image = new Buffer.from(data.image, 'base64').toString('binary');
+                if(data_vi) {
+                    data_vi.image = new Buffer.from(data_vi.image, 'base64').toString('binary');
 
                     let doctorSpecialty = [];
                     if(location === 'ALL') {
@@ -196,16 +260,34 @@ let getDetailSpecialtyById = (inputId, location) => {
                             attributes: ['doctorId', 'provinceId']
                         })
                     }
+                    data_vi.doctorSpecialty = doctorSpecialty;
+                } else data_vi = {};
+                if(data_en) {
+                    data_en.image = new Buffer.from(data_en.image, 'base64').toString('binary');
 
-                    data.doctorSpecialty = doctorSpecialty;
-                } else data = {}
+                    let doctorSpecialty = [];
+                    if(location === 'ALL') {
+                        doctorSpecialty = await db.Doctor_Info.findAll({
+                            where: {specialtyId: inputId},
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    } else {
+                        doctorSpecialty = await db.Doctor_Info.findAll({
+                            where: {
+                                specialtyId: inputId,
+                                provinceId: location
+                            },
+                            attributes: ['doctorId', 'provinceId']
+                        })
+                    }
+                    data_en.doctorSpecialty = doctorSpecialty;
+                } else data_en = {};
                 resolve({
                     errMessage: "OK!",
                     errCode: 0,
-                    data
-                })
-
-                
+                    data_vi: data_vi,
+                    data_en: data_en
+                }) 
             }
         } catch (error) {
             reject(error)
